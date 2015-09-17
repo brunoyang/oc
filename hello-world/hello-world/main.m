d88d8ae0233993d92b98dd43ed3cbd01894e5f51
bruno yang <ygch1993@gmail.com>
2015-09-17 23:33:24 +0800

    add class proj

---
+++ hello-world/hello-world/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  hello-world
+//
+//  Created by bruno on 15/9/17.
+//  Copyright © 2015年 bruno. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
